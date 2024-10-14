c7f83d8b55cc27ea75579615e177907d289c1b89
cornerloan <connorlowe21@gmail.com>
2024-10-14 00:53:29 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,23 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game xD";
+const gameName = "DinoNuggie Clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerText = "Click for 🦖";
+app.append(button);
+
+let counter: number = 0;
+const counterReport = document.createElement("div");
+counterReport.innerText = "Total DinoNuggies: " + counter;
+app.append(counterReport)
+
+button.addEventListener("click", function (e) {
+    counter++;
+    counterReport.innerText = "Total DinoNuggies: " + counter;
+});
