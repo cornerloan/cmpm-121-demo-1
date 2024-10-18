a4bcd4e4e42375dbb45840effca9cb95b8cab714
cornerloan <connorlowe21@gmail.com>
2024-10-17 22:49:19 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,13 @@ app.append(button);
 //Step 2
 let counter: number = 0;
 const counterReport = document.createElement("div");
-counterReport.innerText = "Total DinoNuggies: " + counter;
+counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
 app.append(counterReport)
 
 button.addEventListener("click", function () {
     counter++;
     counterReport.innerText = "Total DinoNuggies: " + counter;
+    checkUpgradeAvailable();
 });
 
 
@@ -37,6 +38,7 @@ function autoClicker() {
 setInterval(autoClicker, 1000);
 */
 
+let growthRate: number = 0;
 
 //Step 4
 let lastTime = Date.now();
@@ -44,11 +46,31 @@ let lastTime = Date.now();
 function updateCounter() {
     const currentTime = Date.now();
     const deltaTime = (currentTime - lastTime) / 1000;
-    counter += deltaTime;
+    counter += deltaTime * growthRate;
     lastTime = currentTime;
 
     counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
+    checkUpgradeAvailable();
     requestAnimationFrame(updateCounter)
 }
 
 requestAnimationFrame(updateCounter);
+
+//Step 5
+const upgradeButton = document.createElement("button");
+upgradeButton.innerText = "Buy 1 Growth Per Second\nCost: 10 DinoNuggies";
+upgradeButton.disabled = true;
+app.append(upgradeButton);
+
+function checkUpgradeAvailable() {
+    upgradeButton.disabled = counter < 10;
+}
+
+upgradeButton.addEventListener("click", function() {
+    if (counter >= 10){
+        counter -= 10;
+        growthRate += 1;
+        counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
+        checkUpgradeAvailable();
+    }
+});
