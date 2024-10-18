b2e0a3ea0c7a41197284e3517ad909d857e2f865
cornerloan <connorlowe21@gmail.com>
2024-10-17 23:48:24 -0700

    Step 8 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "DinoNuggie Clicker";
+const gameName = "🦖DinoNuggie🦖 Clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
@@ -11,19 +11,19 @@ app.append(header);
 
 //Step 1
 const button = document.createElement("button");
-button.innerText = "Click for 🦖";
+button.innerText = "Click for 🦖DinoNuggies🦖";
 app.append(button);
 
 
 //Step 2
 let counter: number = 0;
 const counterReport = document.createElement("div");
-counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
+counterReport.innerText = "Total 🦖DinoNuggies🦖: " + Math.trunc(counter);
 app.append(counterReport)
 
 button.addEventListener("click", function () {
     counter++;
-    counterReport.innerText = "Total DinoNuggies: " + counter;
+    counterReport.innerText = "Total 🦖DinoNuggies🦖: " + counter;
     checkUpgradeAvailable();
 });
 
@@ -49,8 +49,8 @@ function updateCounter() {
     counter += deltaTime * growthRate;
     lastTime = currentTime;
 
-    counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
-    growthText.innerText = "Current growth rate: " + growthRate.toFixed(1) + " DinoNuggies/sec";
+    counterReport.innerText = "Total 🦖DinoNuggies🦖: " + Math.trunc(counter);
+    growthText.innerText = "Current growth rate: " + growthRate.toFixed(1) + " 🦖DinoNuggies🦖/sec";
     checkUpgradeAvailable();
     requestAnimationFrame(updateCounter)
 }
@@ -60,13 +60,13 @@ requestAnimationFrame(updateCounter);
 //Step 6
 
 const growthText = document.createElement("div");
-growthText.innerText = "Current growth rate: " + growthRate.toFixed(1) + " DinoNuggies/sec";
+growthText.innerText = "Current growth rate: " + growthRate.toFixed(1) + " 🦖DinoNuggies🦖/sec";
 app.append(growthText);
 
 const upgrade1Button = document.createElement("button");
 let upgrade1Price: number = 10;
 
-upgrade1Button.innerText = "Buy 0.1 Growth Per Second\nCost: " + upgrade1Price.toFixed(2) + " DinoNuggies";
+upgrade1Button.innerText = "Buy ⛏️Archeologist⛏️\n0.1 Growth Per Second\nCost: " + upgrade1Price.toFixed(2) + " 🦖DinoNuggies🦖";
 upgrade1Button.disabled = true;
 app.append(upgrade1Button);
 upgrade1Button.addEventListener("click", function() {
@@ -74,15 +74,15 @@ upgrade1Button.addEventListener("click", function() {
         counter -= upgrade1Price;
         growthRate += 0.1;
         upgrade1Price *= 1.15;
-        counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
-        upgrade1Button.innerText = "Buy 0.1 Growth Per Second\nCost: " + upgrade1Price.toFixed(2) + " DinoNuggies";
+        counterReport.innerText = "Total 🦖DinoNuggies🦖: " + Math.trunc(counter);
+        upgrade1Button.innerText = "Buy ⛏️Archeologist⛏️\n0.1 Growth Per Second\nCost: " + upgrade1Price.toFixed(2) + " 🦖DinoNuggies🦖";
         checkUpgradeAvailable();
     }
 });
 
 const upgrade2Button = document.createElement("button");
 let upgrade2Price: number = 100;
-upgrade2Button.innerText = "Buy 2 Growth Per Second\nCost: " + upgrade2Price.toFixed(2) + " DinoNuggies";
+upgrade2Button.innerText = "Buy 👨‍🍳Chef👨‍🍳\n2 Growth Per Second\nCost: " + upgrade2Price.toFixed(2) + " 🦖DinoNuggies🦖";
 upgrade2Button.disabled = true;
 app.append(upgrade2Button);
 upgrade2Button.addEventListener("click", function() {
@@ -90,15 +90,15 @@ upgrade2Button.addEventListener("click", function() {
         counter -= upgrade2Price;
         growthRate += 2;
         upgrade2Price *= 1.15;
-        counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
-        upgrade2Button.innerText = "Buy 2 Growth Per Second\nCost: " + upgrade2Price.toFixed(2) + " DinoNuggies";
+        counterReport.innerText = "Total 🦖DinoNuggies🦖: " + Math.trunc(counter);
+        upgrade2Button.innerText = "Buy 👨‍🍳Chef👨‍🍳\n2 Growth Per Second\nCost: " + upgrade2Price.toFixed(2) + " 🦖DinoNuggies🦖";
         checkUpgradeAvailable();
     }
 });
 
 const upgrade3Button = document.createElement("button");
 let upgrade3Price: number = 1000;
-upgrade3Button.innerText = "Buy 50 Growth Per Second\nCost: " + upgrade3Price + " DinoNuggies";
+upgrade3Button.innerText = "Buy 🏭DinoNuggie Factory🏭\n50 Growth Per Second\nCost: " + upgrade3Price + " 🦖DinoNuggies🦖";
 upgrade3Button.disabled = true;
 app.append(upgrade3Button);
 upgrade3Button.addEventListener("click", function() {
@@ -106,8 +106,8 @@ upgrade3Button.addEventListener("click", function() {
         counter -= upgrade3Price;
         growthRate += 50;
         upgrade3Price *= 1.15;
-        counterReport.innerText = "Total DinoNuggies: " + Math.trunc(counter);
-        upgrade3Button.innerText = "Buy 50 Growth Per Second\nCost: " + upgrade3Price + " DinoNuggies";
+        counterReport.innerText = "Total 🦖DinoNuggies🦖: " + Math.trunc(counter);
+        upgrade3Button.innerText = "Buy 🏭DinoNuggie Factory🏭\n50 Growth Per Second\nCost: " + upgrade3Price + " 🦖DinoNuggies🦖";
         checkUpgradeAvailable();
     }
 });
